7847b07b51e42b7c68297bcb13ad76b833e85eb9
Antariksh <ameesingh.amazing.antariksh@gmail.com>
2026-08-07 16:56:40 +0530

    checking connectivity of pi

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print(" Projectile Tracking System")
+    print(" First end-to-end Git test!")
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
